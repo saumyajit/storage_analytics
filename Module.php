66ed6860bb09f556cb1572095b16397aa3e4ce10
Saumyajit <saumyajit@users.noreply.github.com>
2025-12-23 12:00:45 +0530

    Create Module.php

---
+++ Module.php
@@ -0,0 +1,20 @@
+<?php
+class StorageAnalytics extends \Zabbix\Core\CModule {
+    
+    public function init(): void {
+        // Register module routes
+        $this->addRoute('storage.analytics', 'CControllerStorageAnalytics');
+        
+        // Add menu item
+        $this->addMenuItem('Monitoring', [
+            'label' => _('Storage Analytics'),
+            'url' => 'zabbix.php?action=storage.analytics',
+            'icon' => 'icon-storage-analytics',
+            'order' => 300
+        ]);
+    }
+    
+    public function onTerminate(): void {
+        // Cleanup if needed
+    }
+}
